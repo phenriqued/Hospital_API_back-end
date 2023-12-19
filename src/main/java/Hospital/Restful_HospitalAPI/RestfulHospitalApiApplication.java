c9c7843881a58fbcd6d870486539b63664760247
Pedro <pedrodias3737@gmail.com>
2023-12-19 09:57:56 -0300

    Piloto.

---
+++ src/main/java/Hospital/Restful_HospitalAPI/RestfulHospitalApiApplication.java
@@ -0,0 +1,13 @@
+package Hospital.Restful_HospitalAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulHospitalApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestfulHospitalApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Hospital/Restful_HospitalAPI/RestfulHospitalApiApplicationTests.java
@@ -0,0 +1,13 @@
+package Hospital.Restful_HospitalAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulHospitalApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
